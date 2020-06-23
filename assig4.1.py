e560424400cb9a77a793fbf72a0fd39e925d5b82
seershikab <67177758+seershikab@users.noreply.github.com>
2020-06-23 09:21:29 +0530

    Add files via upload

---
+++ assig4.1.py
@@ -0,0 +1,9 @@
+def maximum(a,b,c):
+	if(>=b)and(a>=c):
+		print("maximum is",c)
+		elif(b>=a)and(b>=c):
+			print("maximum is",b)
+			else:
+				print("maximum is",c)
+				return maximum
+				maximum(10,20,30)
